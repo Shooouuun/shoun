3856f29ec11d9991fb1ad3a146ff2ef9168955bb
SHOUN <Shooouuun@gmail.com>
2019-01-25 02:17:20 +0300

    Edit class

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,21 @@
-var link = document.querySelector(".modal-open");
+var open = document.querySelector(".modal-open");
 var popup = document.querySelector(".modal-login");
 var close = document.querySelector(".modal-close");
 var focusblock = document.querySelector(".main-nav__list-modal");
 
-link.addEventListener("click", function (evt) {
+open.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  link.classList.add("modal-open-1");
-  close.classList.add("modal-close-1");
+  open.classList.toggle("dropdown-menu__button--close");
+  close.classList.toggle("dropdown-menu__button dropdown-menu__button--open");
   focusblock.classList.add("main-nav__list-modal-1");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-  link.classList.remove("modal-open-1");
-  close.classList.remove("modal-close-1");
+  open.classList.toggle("dropdown-menu__button dropdown-menu__button--open");
+  close.classList.toggle("dropdown-menu__button dropdown-menu__button--close");
   focusblock.classList.remove("main-nav__list-modal-1");
 
   popup.classList.remove("modal-error");
