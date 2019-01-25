3e957d2bdf58607409908d448add3c96434679cc
SHOUN <Shooouuun@gmail.com>
2019-01-25 07:53:07 +0300

    the toggle switch did not work

--- js/scripts.js
+++ js/scripts.js
@@ -6,16 +6,16 @@ var focusblock = document.querySelector(".main-nav__list-modal");
 open.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.add("modal-show");
-  open.classList.toggle("dropdown-menu__button--close");
-  close.classList.toggle("dropdown-menu__button--open");
+  open.classList.add("dropdown-menu__button--close");
+  close.classList.add("dropdown-menu__button--open");
   focusblock.classList.add("main-nav__list-modal-1");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   popup.classList.remove("modal-show");
-  open.classList.toggle("dropdown-menu__button--open");
-  close.classList.toggle("dropdown-menu__button--close");
+  open.classList.remove("dropdown-menu__button--open");
+  close.classList.remove("dropdown-menu__button--close");
   focusblock.classList.remove("main-nav__list-modal-1");
 
   popup.classList.remove("modal-error");
