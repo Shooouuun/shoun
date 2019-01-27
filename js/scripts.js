d7f97b37905d134c0bc585d9ab369a8f655d73d2
shoun <shooouuun@gmail.com>
2019-01-28 00:58:26 +0300

    Переделана навигация. Убрано моделька. Убрано лишнее в коде.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,22 @@
 var open = document.querySelector(".dropdown-menu__button--open");
 var close = document.querySelector(".dropdown-menu__button--close");
-var dropdown = document.querySelector(".modal-login");
-var focusblock = document.querySelector(".main-nav__list-modal");
+var dropdown = document.querySelector(".main-header");
+var focusblock = document.querySelector(".main-nav__wrapper");
 
 open.addEventListener("click", function (evt) {
   evt.preventDefault();
   open.classList.add("dropdown-menu__button--close1");
   close.classList.add("dropdown-menu__button--open1");
-  dropdown.classList.add("modal-show");
-  focusblock.classList.add("main-nav__list-modal-1");
+  dropdown.classList.add("main-header1");
+  focusblock.classList.add("main-nav__wrapper1");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
   open.classList.remove("dropdown-menu__button--close1");
   close.classList.remove("dropdown-menu__button--open1");
-  dropdown.classList.remove("modal-show");
-  focusblock.classList.remove("main-nav__list-modal-1");
+  dropdown.classList.remove("main-header1");
+  focusblock.classList.remove("main-nav__wrapper1");
 
   dropdown.classList.remove("modal-error");
 });
